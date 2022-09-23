839ac300ebfffd2f71bb7d781eefe2b58fcbe2e3
AhmedMousa-ag <ahmedkaremag@gmail.com>
2022-09-23 16:06:52 +0200

    Reformatted

--- design_experiment.py
+++ design_experiment.py
@@ -3,59 +3,60 @@ import tensorflow as tf
 from tensorflow.keras.layers import Dense
 from Utils.mlflow import mlflow_track
 import mlflow
-from sklearn.metrics import accuracy_score,f1_score
+from sklearn.metrics import accuracy_score, f1_score
 from xgboost import XGBClassifier
 
 
 @mlflow_track
-def xgb_exp(x_train, y_train, x_test, y_test,data_version):
+def xgb_exp(x_train, y_train, x_test, y_test, data_version):
     print("Starting XGBoost Classifier Experiement")
     mlflow.xgboost.autolog()
-    mlflow.log_param("Data Version",data_version)
+    mlflow.log_param("Data Version", data_version)
     model = XGBClassifier()
     model = model.fit(x_train, y_train)
     y_pred = model.predict(x_test)
-    acc = accuracy_score(y_test,y_pred)
-    mlflow.log_metric("accuracy",acc)
-    mlflow.log_metric("f1_score",f1_score)
+    acc = accuracy_score(y_test, y_pred)
+    mlflow.log_metric("accuracy", acc)
+    mlflow.log_metric("f1_score", f1_score)
 
 
 @mlflow_track
-def skl_train(x_train, y_train, x_test, y_test,data_version,model):
+def skl_train(x_train, y_train, x_test, y_test, data_version, model):
     mlflow.sklearn.autolog()
-    mlflow.log_param("Data Version",data_version)
+    mlflow.log_param("Data Version", data_version)
     model = model()
-    model.fit(x_train,y_train)
+    model.fit(x_train, y_train)
     y_pred = model.predict(x_test)
-    acc = accuracy_score(y_test,y_pred)
-    mlflow.log_metric("accuracy",acc)
-    mlflow.log_metric("f1_score",f1_score)
+    acc = accuracy_score(y_test, y_pred)
+    mlflow.log_metric("accuracy", acc)
+    mlflow.log_metric("f1_score", f1_score)
 
-def skl_exp(x_train, y_train, x_test, y_test,data_version,models):
+
+def skl_exp(x_train, y_train, x_test, y_test, data_version, models):
     print("Starting Sklearn Experiement")
     model_num = 0
     for model in models:
-        model_num +=1
+        model_num += 1
         print(model_num)
-        skl_train(x_train, y_train, x_test, y_test,data_version,model)
+        skl_train(x_train, y_train, x_test, y_test, data_version, model)
 
 
-def build_model(num_layers=3,nuerons_num=30):
+def build_model(num_layers=3, nuerons_num=30):
     model = tf.keras.Sequential()
-    for _ in range(num_layers-1):
+    for _ in range(num_layers - 1):
         model.add(Dense(nuerons_num))
-    model.add(Dense(1,activation='sigmoid'))
-    model.compile(loss='binary_crossentropy',optimizer='adam',metrics="accuracy")
+    model.add(Dense(1, activation='sigmoid'))
+    model.compile(loss='binary_crossentropy', optimizer='adam', metrics="accuracy")
     return model
 
 
 @mlflow_track
-def tf_exper(x_train, y_train, x_test, y_test,data_version):
+def tf_exper(x_train, y_train, x_test, y_test, data_version):
     print("Starting Tensorflow Experiement")
     mlflow.tensorflow.autolog()
-    mlflow.log_param("Data Version",data_version)
+    mlflow.log_param("Data Version", data_version)
     model = build_model()
-    model.fit(x_train,y_train)
-    loss,acc = model.evaluate(x_test,y_test)
-    mlflow.log_metric("accuracy",acc)
-    #mlflow.log_metric("f1_score",f1_score) # Tensorflow doesn't support f1_score, so we have to write it if we need it, inherit from keras.metrics
+    model.fit(x_train, y_train)
+    loss, acc = model.evaluate(x_test, y_test)
+    mlflow.log_metric("accuracy", acc)
+    # mlflow.log_metric("f1_score",f1_score) # Tensorflow doesn't support f1_score, so we have to write it if we need it, inherit from keras.metrics
--- main.py
+++ main.py
@@ -2,14 +2,14 @@ from Utils.config import load_config_file
 from Utils.data import load_data
 import mlflow
 from design_experiment import tf_exper, skl_exp, xgb_exp
+
 config_file = load_config_file()
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.linear_model import LogisticRegression, RidgeClassifier, SGDClassifier
 from sklearn.neighbors import NearestCentroid
 from sklearn.naive_bayes import GaussianNB
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.neural_network import MLPClassifier # This is interesting because it's Neural Network
-
+from sklearn.neural_network import MLPClassifier  # This is interesting because it's Neural Network
 
 tracking_uri = config_file["tracking_uri"]
 exp_name = config_file["exp_name"]
@@ -17,18 +17,14 @@ exp_name = config_file["exp_name"]
 mlflow.set_tracking_uri(tracking_uri)
 mlflow.set_experiment(exp_name)
 
-models = [DecisionTreeClassifier,LogisticRegression,RidgeClassifier, SGDClassifier,
-            NearestCentroid,RandomForestClassifier, GaussianNB, MLPClassifier]
-
-
+models = [DecisionTreeClassifier, LogisticRegression, RidgeClassifier, SGDClassifier,
+          NearestCentroid, RandomForestClassifier, GaussianNB, MLPClassifier]
 
 main_path = config_file["paths"]["prep_data"]
-
 
 for data_version in config_file["prep_data_names"]:
     x_train, y_train, x_test, y_test = load_data(data_version, main_path)
 
-    skl_exp(x_train, y_train, x_test, y_test,data_version,models)
-    xgb_exp(x_train, y_train, x_test, y_test,data_version)
-    tf_exper(x_train, y_train, x_test, y_test,data_version)
-   
+    skl_exp(x_train, y_train, x_test, y_test, data_version, models)
+    xgb_exp(x_train, y_train, x_test, y_test, data_version)
+    tf_exper(x_train, y_train, x_test, y_test, data_version)
