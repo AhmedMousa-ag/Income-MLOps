e03c511f6cd167dfc7835ea29d3937ef69fc756b
AhmedMousa-ag <ahmedkaremag@gmail.com>
2022-09-23 15:37:04 +0200

    Time to test

--- main.py
+++ main.py
@@ -3,7 +3,12 @@ from Utils.data import load_data
 import mlflow
 from design_experiment import tf_exper, skl_train, xgb_exp
 config_file = load_config_file()
-
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.linear_model import LogisticRegression, RidgeClassifier, SGDClassifier
+from sklearn.neighbors import NearestCentroid
+from sklearn.naive_bayes import GaussianNB
+from sklearn.ensemble import RandomForestClassifier, VotingClassifier
+from sklearn.neural_network import MLPClassifier # This is interesting because it's Neural Network
 
 
 tracking_uri = config_file["tracking_uri"]
@@ -12,7 +17,8 @@ exp_name = config_file["exp_name"]
 mlflow.set_tracking_uri(tracking_uri)
 mlflow.set_experiment("my-experiment-1")
 
-models = []
+models = [DecisionTreeClassifier,LogisticRegression,RidgeClassifier, SGDClassifier,
+            NearestCentroid,RandomForestClassifier, VotingClassifier, GaussianNB, MLPClassifier]
 
 
 
@@ -21,6 +27,8 @@ main_path = config_file["paths"]["prep_data"]
 
 for data_version in config_file["prep_data_names"]:
     x_train, y_train, x_test, y_test = load_data(data_version, main_path)
-    tf_exper(x_train, y_train, x_test, y_test,data_version)
+
+    
     skl_train(x_train, y_train, x_test, y_test,data_version)
     xgb_exp(x_train, y_train, x_test, y_test,data_version)
+    tf_exper(x_train, y_train, x_test, y_test,data_version)
